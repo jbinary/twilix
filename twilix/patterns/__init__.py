6acb69b9878a37de505c896ae3c8a034cd77f737
Dmitry Gribanov <dmitry.gribanov@hidevlab.com>
2016-11-03 17:04:25 +0300

    Set keepalive_period unconditionally and don't use hasattr later when start/stop method invoking

--- twilix/patterns/__init__.py
+++ twilix/patterns/__init__.py
@@ -6,19 +6,17 @@ class BasePattern(object):
     def __init__(self, myjid, keepalive_period=None):
         """ :param myjid: The jid of the component.
             :param keepalive_period: The number of seconds between sending of keepalives."""
-
         self.myjid = internJID(myjid)
-        if keepalive_period is not None:
-            self.keepalive_period = keepalive_period
-            self.keepalive_send_task = task.LoopingCall(self.send_keepalive)
+        self.keepalive_period = keepalive_period
+        self.keepalive_send_task = task.LoopingCall(self.send_keepalive)
 
     def send_keepalive(self):
         self.xmlstream.transport.write(' ')
 
     def startSendingKeepalives(self):
-        if hasattr(self, 'keepalive_period'):
+        if self.keepalive_period:
             self.keepalive_send_task.start(self.keepalive_period)
 
     def stopSendingKeepalives(self):
-        if hasattr(self, 'keepalive_period'):
+        if self.keepalive_period:
             self.keepalive_send_task.stop()
